2cecb03e96ddaad3595245f0ef40b148d6a2ac1a
terrablue <102580937+terrablue@users.noreply.github.com>
2022-07-31 23:21:44 +0200

    hs: add error case and tests

--- src/interpreters/hs.js
+++ src/interpreters/hs.js
@@ -1,8 +1,23 @@
-import {execSync} from "node:child_process";
+import {spawnSync} from "node:child_process";
 
-const command_length = 6;
+const types = {
+  ok: {
+    source: "stdout",
+    from: 6,
+  },
+  err: {
+    source: "stderr",
+    from: 14,
+  },
+};
 
-export default syntax => {
-  const output = execSync(`ghci <<< '${syntax}'`).toString("utf8");
-  return output.split("\n").at(1).slice(command_length);
+const format = (result, type) => {
+  const {source, from} = types[type];
+  return `(${type}) ${result[source].toString().split("\n").at(1).slice(from)}`;
+};
+
+export default input => {
+  const command = "ghci";
+  const result = spawnSync(command, {input});
+  return format(result, result.stderr.toString().length === 0 ? "ok" : "err");
 };
---
+++ src/interpreters/hs.spec.js
@@ -0,0 +1,15 @@
+import {Test} from "debris";
+import hs from "./hs.js";
+
+const test = new Test();
+
+test.case("success", assert => {
+  assert(hs("0 + 1")).equals("(ok) 1");
+});
+
+test.case("failure", assert => {
+  assert(hs("0 + !")).equals("(err) 1:5: error: parse error on input ‘!’");
+});
+
+export default test;
+
