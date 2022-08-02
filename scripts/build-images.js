aaa1da02d11c376a043604bf22ad665c6103bd39
terrablue <102580937+terrablue@users.noreply.github.com>
2022-08-03 01:29:10 +0200

    update runtime-compat dep

--- scripts/build-images.js
+++ scripts/build-images.js
@@ -1,9 +1,9 @@
-import {Path, File} from "runtime-compat/filesystem";
+import {Path} from "runtime-compat/filesystem";
 import {spawnSync} from "node:child_process";
 
 const interpreters = Path.resolve().join("src", "interpreters");
 
-for (const {directory} of await File.collect(interpreters, "Dockerfile")) {
+for (const {directory} of await interpreters.file.collect("Dockerfile")) {
   const command = "docker";
   const args = ["build", "-t", `gpbot/${directory.name}`, "."];
   const options = {cwd: directory.path};
--- src/bot.js
+++ src/bot.js
@@ -1,21 +1,21 @@
 import irc from "irc-upd";
 import {config} from "dotenv";
-import {File, Path} from "runtime-compat/filesystem";
+import {Path} from "runtime-compat/filesystem";
 
 config();
 
 const {irc_network, irc_user, irc_channels} = process.env;
 const channels = irc_channels.split(";");
 
-const {directory} = new Path(import.meta.url);
+const interpreters = new Path(import.meta.url).directory.join("interpreters");
 
 const client = new irc.Client(irc_network, irc_user, {channels});
 
 const run = async (interpreter, syntax, recipient) => {
-  const path = directory.join("interpreters", interpreter, "run.js");
-  if (await new File(path).exists) {
+  const {file} = interpreters.join(interpreter, "run.js");
+  if (await file.exists) {
     try {
-      const handler = (await import(path.path)).default;
+      const handler = (await import(file.path)).default;
       client.say(recipient, handler(syntax));
     } catch (error) {
       // ignore
