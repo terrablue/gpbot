b3ab2bbefd9868ca00f07d7a17250ed6f59cb73f
terrablue <102580937+terrablue@users.noreply.github.com>
2023-02-21 13:45:37 +0200

    gpt: catch exceptions

--- src/gpt.js
+++ src/gpt.js
@@ -2,5 +2,9 @@ import {Configuration, OpenAIApi} from "openai";
 
 export default async (apiKey, config) => {
   const openai = new OpenAIApi(new Configuration({apiKey}));
-  return (await openai.createCompletion(config)).data.choices[0].text;
+  try {
+    return (await openai.createCompletion(config)).data.choices[0].text;
+  } catch (error) {
+    return `(err) openai API unavailable (${error.response.statusText})`;
+  }
 };
