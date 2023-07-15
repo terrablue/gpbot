5ffa1b9de924d21a24bdf6282c4c59fc619e60a7
terrablue <102580937+terrablue@users.noreply.github.com>
2023-07-15 20:30:57 +0300

    support more than one channel

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import {Path} from "runtime-compat/fs";
 import crypto from "runtime-compat/crypto";
 import {Response, Status} from "runtime-compat/http";
 import {valmap, from, to} from "runtime-compat/object";
-import {assert} from "runtime-compat/dyndef";
 
 const conf = await new Path(import.meta.url).up(2).join("conf.json").json();
 const encoder = new TextEncoder("utf-8");
@@ -11,6 +10,7 @@ const hash = "SHA-256";
 const algorithm = {name: "HMAC", hash};
 
 const secrets = valmap(conf.github, ({secret}) => secret);
+console.log(secrets);
 const keys = from(await Promise.all(to(secrets).map(async ([key, value]) =>
   [key, await crypto.subtle.importKey("raw", encode(value), algorithm, false,
     ["verify"])])));
@@ -21,12 +21,8 @@ const {baseuri} = conf;
 const htia = string =>
   new Uint8Array(string.match(/[\dA-F]{2}/gui).map(hex => parseInt(hex, 16)));
 
-const verify = (body, signature) => {
-  const result = to(keys).find(([, key]) =>
-    crypto.subtle.verify(algorithm.name, key, htia(signature), encode(body))
-  );
-  return result?.[0];
-};
+const verify = (body, signature, repository) =>
+  crypto.subtle.verify(algorithm.name, keys[repository], htia(signature), encode(body));
 
 const preface = (repository, color) => `\x03${color},01${repository}\x03 ::`;
 const bold = message => `\x02${message}\x02`;
@@ -34,9 +30,9 @@ const bold = message => `\x02${message}\x02`;
 const events = {
   async issues({action, issue}, Link) {
     if (action === "opened") {
-      const {html_url, user: {login}, number} = issue;
+      const {html_url, user: {login}, title} = issue;
       const target = `${baseuri}/${await Link.shorten(html_url)}`;
-      return `${bold(login)} opened issue #${number} [${target}]`;
+      return `${bold(login)} opened issue ${bold(title)} [${target}]`;
     }
   },
   push({commits}, Link) {
@@ -60,15 +56,10 @@ export default {
   async post(request) {
     const body = request.body.get();
     const signature = request.headers.get("x-hub-signature-256");
-    const repository = await verify(JSON.stringify(body),
-      signature.slice(hash.length));
-    if (repository !== undefined) {
-      const {repository: {full_name}} = body;
-      assert(repository === full_name, `
-        repository in event data must match repository by secret
-        repository by secret: ${repository}
-        repository in event data: ${full_name}
-      `);
+    const repository = body.repository.full_name;
+    const verified = await verify(JSON.stringify(body),
+      signature.slice(hash.length), repository);
+    if (verified) {
       const event = request.headers.get("x-github-event");
       const name = preface(repository, colors[repository] ?? "14");
       const {Link} = request.store;
