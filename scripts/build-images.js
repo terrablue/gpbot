ac0b51cef93b135627e832f8eeb1dcea458af1a2
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-19 21:36:54 +0200

    fix build scripts

--- scripts/build-images.js
+++ scripts/build-images.js
@@ -3,10 +3,9 @@ import { execSync } from "node:child_process";
 
 const interpreters = File.resolve().join("src", "interpreters");
 
-for (const { directory } of await interpreters.file.collect("Dockerfile")) {
+for (const { directory } of await interpreters.collect("Dockerfile")) {
   const command = "docker";
   const args = ["build", "-t", `gpbot/${directory.name}`, directory.path];
   const cmd = [command, ...args].join(" ");
-  console.info(cmd);
   execSync(cmd, directory.path);
 }
