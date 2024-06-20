29f90a8a48026273d89e53b8f2c0b7c525143b35
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-20 15:48:10 +0200

    reoganize and fix tests

--- src/run.spec.js
+++ src/run.spec.js
@@ -13,110 +13,23 @@ export default test => {
   const ex = syntax => run(`ex> ${syntax}`);
   const rb = syntax => run(`rb> ${syntax}`);
 
-  test.case("0 + 1", async assert => {
-    const result = ["(ok) 1"];
-    assert(await hs("0 + 1")).equals(result);
-    assert(await rs("0 + 1")).equals(result);
-    assert(await py("0 + 1")).equals(result);
-    assert(await js("0 + 1")).equals(result);
-    assert(await ts("0 + 1")).equals(result);
-    assert(await erl("0 + 1")).equals(result);
-    assert(await ml("0 + 1")).equals(["(ok) int = 1"]);
-    assert(await ex("0 + 1")).equals(result);
-    assert(await rb("0 + 1")).equals(result);
-  });
+  const r = {
+    _1: ["(ok) 1"],
+    _1_1: ["(ok) 1 1"],
+    true: ["(ok) true"],
+    True: ["(ok) True"],
+    bool_true: ["(ok) bool = true"],
+    _100d: ["(ok) 100.0"],
+    _100: ["(ok) 100"],
+  };
 
-  test.case("1 > 0", async assert => {
-    const result = {
-      true: ["(ok) true"],
-      True: ["(ok) True"],
-      bool_true: ["(ok) bool = true"],
-    };
-    assert(await hs("1 > 0")).equals(result.True);
-    assert(await rs("1 > 0")).equals(result.true);
-    assert(await py("1 > 0")).equals(result.True);
-    assert(await js("1 > 0")).equals(result.true);
-    assert(await ts("1 > 0")).equals(result.true);
-    assert(await erl("1 > 0")).equals(result.true);
-    assert(await ml("1 > 0")).equals(result.bool_true);
-    assert(await ex("1 > 0")).equals(result.true);
-    assert(await rb("1 > 0")).equals(result.true);
-  });
-
-  test.case("1; 1", async assert => {
-    assert(await rs("1; 1")).equals(["(ok) 1"]);
-    assert(await py("1; 1")).equals(["(ok) 1 1"]);
-    assert(await ts("1; 1")).equals(["(ok) 1"]);
-    assert(await erl("1. 1")).equals(["(ok) 1"]);
-  });
-
-  test.case("0 + !", async assert => {
-    const errorHS = ["(err) 1:6: error: parse error on input `!'"];
-    assert(await hs("0 + !")).equals(errorHS);
-    assert(await py("0 + !")).equals(["(err) SyntaxError: incomplete input"]);
-    assert(await rs("0 + !")).equals(["(err) expected expression, found `}`"]);
-    const errorJS = ["(ok) unexpected token in expression: ')'     at eval:1"];
-    assert(await js("0 + !")).equals(errorJS);
-    const errorTS = ["(err) [eval].ts(1,6): error TS1109: Expression expected."];
-    assert(await ts("0 + !")).equals(errorTS);
-    const errorML = ["(ok) Line 1, characters 6-8:",
-      "1 |  0 + !;;",
-      "          ^^",
-      "Error: Syntax error"];
-    assert(await ml("0 + !")).equals(errorML);
-    const errorRB = ["(err) -e:1: syntax error, unexpected ')'",
-      "puts(0 + !)",
-      "          ^"];
-    assert(await rb("0 + !")).equals(errorRB);
-  });
-
-  test.case("10 + 20 * 4.5", async assert => {
-    const result = {
-      ok100d: ["(ok) 100.0"],
-      ok100: ["(ok) 100"],
-    };
-    assert(await hs("10 + 20*4.5")).equals(result.ok100d);
-    assert(await py("10 + 20*4.5")).equals(result.ok100d);
-    const err = ["(err) [E0277]: cannot multiply `{integer}` by `{float}`"];
-    assert(await rs("10 + 20*4.5")).equals(err);
-    assert(await js("10 + 20*4.5")).equals(result.ok100);
-    assert(await ts("10 + 20*4.5")).equals(result.ok100);
-    assert(await ml("10. +. 20. *. 4.5")).equals(["(ok) float = 100."]);
-    assert(await ex("10 + 20 * 4.5")).equals(result.ok100d);
-  });
-
-  test.case("functions", async assert => {
-    const r = ["(ok) 'foobar'"];
-    assert(await py("foo = lambda x: f'foo{x}'; foo('bar')")).equals(r);
-    assert(await py("foo = lambda x: f\"foo{x}\"; foo(\"bar\")")).equals(r);
-    assert(await ml(`let m o = match o with | Some i -> string_of_int i | None -> "";; m(Some 2)`)).equals(["(ok) string = \"2\""]);
-    assert(await ex("%{values: 1..5} |> Map.get(:values) |> Enum.map(& &1 * 2)")).equals(["(ok) [2, 4, 6, 8, 10]"]);
-  });
-
-  test.case("; at the end", async assert => {
-    const r = ["(ok) \"secret\""];
-    assert(await rs("let guess = \"secret\"; guess")).equals(r);
-    assert(await rs("let guess = \"secret\"; guess;")).equals(r);
-  });
-
-  test.case("quotes", async assert => {
-    const r = ["(ok) hello world"];
-    assert(await ts("'hello world'")).equals(r);
-    assert(await ts("\"hello world\"")).equals(r);
-  });
-
-  test.case("failure", async assert => {
-    assert(await run("js> 0 + 1")).throws();
-    assert(await run("0 + 1")).throws();
-  });
-
-  test.case("split input", async assert => {
-    const r = ["(ok) 1"];
-    assert(await rb("a = 1; a")).equals(r);
-  });
-
-  test.case("multiline", async assert => {
-    const result = [
+  test.case("haskell", async assert => {
+    assert(await hs("0 + 1")).equals(r._1);
+    assert(await hs("1 > 0")).equals(r.True);
+    const error = ["(err) 1:5: error: parse error on input `!'"];
+    assert(await hs("0 + !")).equals(error);
+    assert(await hs("10 + 20*4.5")).equals(r._100d);
+    const multiline = [
       "(ok) type Show :: * -> Constraint",
       "class Show a where",
       "  ...",
@@ -124,11 +37,8 @@ export default test => {
       "  ...",
       "    -- Defined in `GHC.Show'",
     ];
-    assert(await hs(":info show")).equals(result);
-  });
-
-  test.case("limit", async assert => {
-    const result = [
+    assert(await hs(":info show")).equals(multiline);
+    const list = [
       "(ok) type Show :: * -> Constraint",
       "class Show a where",
       "  showsPrec :: Int -> a -> ShowS",
@@ -136,6 +46,90 @@ export default test => {
       "  showList :: [a] -> ShowS",
       "  {-# MINIMAL showsPrec | show #-}",
     ];
-    assert(await hs(":info Show")).equals(result);
+    assert(await hs(":info Show")).equals(list);
+  });
+
+  test.case("rust", async assert => {
+    assert(await rs("0 + 1")).equals(r._1);
+    assert(await rs("1 > 0")).equals(r.true);
+    assert(await rs("1; 1")).equals(r._1);
+    const error0 = ["(err) expected expression, found `}`"];
+    assert(await rs("0 + !")).equals(error0);
+    const error1 = ["(err) [E0277]: cannot multiply `{integer}` by `{float}`"];
+    assert(await rs("10 + 20*4.5")).equals(error1);
+    const secret = ["(ok) \"secret\""];
+    assert(await rs("let guess = \"secret\"; guess")).equals(secret);
+    assert(await rs("let guess = \"secret\"; guess;")).equals(secret);
+  });
+
+  test.case("python", async assert => {
+    assert(await py("0 + 1")).equals(r._1);
+    assert(await py("1 > 0")).equals(r.True);
+    assert(await py("1; 1")).equals(r._1_1);
+    assert(await py("0 + !")).equals(["(err) SyntaxError: invalid syntax"]);
+    assert(await py("10 + 20*4.5")).equals(r._100d);
+    const fb = ["(ok) 'foobar'"];
+    assert(await py("foo = lambda x: f'foo{x}'; foo('bar')")).equals(fb);
+    assert(await py("foo = lambda x: f\"foo{x}\"; foo(\"bar\")")).equals(fb);
+  });
+
+  test.case("javascript", async assert => {
+    assert(await js("0 + 1")).equals(r._1);
+    assert(await js("1 > 0")).equals(r.true);
+    const error = ["(err)       at /app/[eval]:1:28"];
+    assert(await js("0 + !")).equals(error);
+    assert(await js("10 + 20*4.5")).equals(r._100);
+  });
+
+  test.case("typescript", async assert => {
+    assert(await ts("0 + 1")).equals(r._1);
+    assert(await ts("1 > 0")).equals(r.true);
+    assert(await ts("1; 1")).equals(r._1);
+    const error = ["(err) [eval].ts(1,6): error TS1109: Expression expected."];
+    assert(await ts("0 + !")).equals(error);
+    assert(await ts("10 + 20*4.5")).equals(r._100);
+    const hw = ["(ok) hello world"];
+    assert(await ts("'hello world'")).equals(hw);
+    assert(await ts("\"hello world\"")).equals(hw);
+  });
+
+  test.case("erlang", async assert => {
+    assert(await erl("0 + 1")).equals(r._1);
+    assert(await erl("1 > 0")).equals(r.true);
+    assert(await erl("1. 1")).equals(r._1);
+  });
+
+  test.case("ocaml", async assert => {
+    assert(await ml("0 + 1")).equals(["(ok) int = 1"]);
+    assert(await ml("1 > 0")).equals(r.bool_true);
+    const error = ["(ok) Line 1, characters 5-7:",
+      "1 | 0 + !;;",
+      "         ^^",
+      "Error: Syntax error"];
+    assert(await ml("0 + !")).equals(error);
+    assert(await ml("10. +. 20. *. 4.5")).equals(["(ok) float = 100."]);
+    assert(await ml(`let m o = match o with | Some i -> string_of_int i | None -> "";; m(Some 2)`)).equals(["(ok) string = \"2\""]);
+  });
+
+  test.case("elixir", async assert => {
+    assert(await ex("0 + 1")).equals(r._1);
+    assert(await ex("1 > 0")).equals(r.true);
+    assert(await ex("10 + 20 * 4.5")).equals(r._100d);
+    assert(await ex("%{values: 1..5} |> Map.get(:values) |> Enum.map(& &1 * 2)")).equals(["(ok) [2, 4, 6, 8, 10]"]);
+  });
+
+  test.case("ruby", async assert => {
+    assert(await rb("0 + 1")).equals(r._1);
+    assert(await rb("1 > 0")).equals(r.true);
+    const error = ["(err) -e:1: syntax error, unexpected ')'",
+      "puts(0 + !)",
+      "          ^"];
+    assert(await rb("0 + !")).equals(error);
+    assert(await rb("a = 1; a")).equals(r._1);
+  });
+
+  test.case("failure", async assert => {
+    assert(await run("js> 0 + 1")).throws();
+    assert(await run("0 + 1")).throws();
   });
 };
