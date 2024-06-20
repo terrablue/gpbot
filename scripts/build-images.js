6e38bb1b70e53048e11a681a02b0963a042efbb8
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-20 11:32:44 +0200

    update build script

--- scripts/build-images.js
+++ scripts/build-images.js
@@ -3,9 +3,12 @@ import { execSync } from "node:child_process";
 
 const interpreters = File.resolve().join("src", "interpreters");
 
+const command = "docker";
+// make sure the latest superarch/minarch image is pulled
+execSync(`${command} pull superarch/minarch`, { stdio: "inherit" });
+
 for (const { directory } of await interpreters.collect("Dockerfile")) {
-  const command = "docker";
   const args = ["build", "-t", `gpbot/${directory.name}`, directory.path];
   const cmd = [command, ...args].join(" ");
-  execSync(cmd, directory.path);
+  execSync(cmd, { cwd: directory.path, stdio: "inherit" });
 }
